6e4c3b065584f53fc589982d58cf1f02c3c76a58
ke <baiheinet@gmail.com>
2024-06-12 08:52:20 +0800

    Update Header.vue
    change github link

--- components/layouts/Header.vue
+++ components/layouts/Header.vue
@@ -63,7 +63,7 @@ const showMenu = ref(false)
                 href="/dashboard"
               >Dashboard</a>
               <a
-                href="https://github.com/ccbikai/sink"
+                href="https://github.com/baiheinet/Sink"
                 target="_blank"
                 class="inline-flex items-center w-full px-6 py-3 text-sm font-medium leading-4 text-white bg-gray-900 md:px-3 md:w-auto md:rounded-full hover:bg-gray-800 focus:outline-none md:focus:ring-2 focus:ring-0 focus:ring-offset-2 focus:ring-gray-800"
               >
